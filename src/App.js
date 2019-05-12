1a5519c99457a317db7849df8e5d0ddce8f32cd9
Tevon Strand-Brown <tevon.strandbrown@gmail.com>
2019-05-12 13:23:51 -0700

    Added text and finished

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import TextScramble from './Text.js';
+import { cards } from './cards.js';
+const title = "Black and White Cards";
+
+
+const description = ["The purpose of these cards is to jog the creative mindset. Sometimes we get stuck in the way we think about thing, the hope is that in considering something tangential, we get a different view of the problem at home. These were developed from interviews during the Stanford class Beyond Pink and Blue, and thus have a specific focus on gender in tech."]
+
 
 class App extends Component {
-  render() {
+
+  constructor(props){
+    super(props);
+    this.state ={
+      cardsActive: false,
+      counter: 0,
+      card: cards[Math.floor(Math.random() * cards.length)],
+    }
+    this.selectCard = this.selectCard.bind(this);
+  }
+
+  componentDidMount(){
+  }
+
+  selectCard(){
+    const card = Math.floor(Math.random() * cards.length)
+    this.setState({
+      card: cards[card],
+    });
+    console.log('Cleckied')
+  }
+
+  renderCards(){
+    return (<div className="CardContainer"
+    >
+    <div className="Card"
+      onClick={() => this.setState({
+        card: cards[Math.floor(Math.random() * cards.length)],
+      })}
+      >{this.state.card}</div>
+  </div>)
+}
+
+getCard(){
+  return description.map((item) => {
+    return (<p key={item} className="Description">{item}</p>)
+  });
+}
+
+render() {
+  if(this.state.cardsActive){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="Container">
+          {this.renderCards()}
+        </div>
       </div>
-    );
+    )
   }
+  return (
+    <div className="App">
+      <div className="Container">
+        <h1 className="Header"
+          >
+          {title}
+        </h1>
+        <div>
+          {this.getCard()}
+        </div>
+        <div className="ButtonContainer">
+          <button className="Button" onClick={() => this.setState({cardsActive: true,})}>The Cards</button>
+        </div>
+      </div>
+    </div>
+  );
+}
 }
 
 export default App;
---
+++ src/cards.js
@@ -0,0 +1,8 @@
+export const cards = [
+  "pay close attention to the positive things and amplify them. We have an predisposition to focus on, and attempt to minimize, the negative.",
+  "who influences the decisions they make?",
+  "write about it.",
+  "be non-consensus and right.",
+  "What if it/they switched genders? What would change?",
+  
+];
